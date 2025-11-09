529aa010c98312c14ca22bfb474c806e58abc47b
Michiel Nijs <michiel5.nijs@gmail.com>
2025-11-09 21:36:13 +0100

    Poosh

--- src/main/java/be/ucll/crafsmanship/command/gamecontroller/PS5Controller.java
+++ src/main/java/be/ucll/crafsmanship/command/gamecontroller/PS5Controller.java
@@ -1,5 +1,37 @@
 package be.ucll.crafsmanship.command.gamecontroller;
 
 public class PS5Controller {
-    // todo
+    Command command;
+
+    public void setCrossButton(Command crossButton) {
+        this.command = crossButton;
+    }
+
+    public void pressCross() {
+        command.execute();
+    }
+
+    public void setTriangleButton(Command triangleButton) {
+        this.command = triangleButton;
+    }
+
+    public void pressTriangle() {
+        command.execute();
+    }
+
+    public void setSquareButton(Command squareButton) {
+        this.command = squareButton;
+    }
+
+    public void pressSquare() {
+        command.execute();
+    }
+
+    public void setCircleButton(Command circleButton) {
+        this.command = circleButton;
+    }
+
+    public void pressCircle() {
+        command.execute();
+    }
 }
